ac9cc0b93c874912fd59632a61c03df2c2df5bfe
boiarshinov <artem.borya@gmail.com>
2022-08-17 16:03:27 +0300

    Add test

--- src/main/java/dev/mirplatform/RomanDigits.java
+++ src/main/java/dev/mirplatform/RomanDigits.java
@@ -1,4 +1,15 @@
 package dev.mirplatform;
 
+import java.util.Map;
+
 public class RomanDigits {
+
+
+    private static final Map<Integer, String> NUMBERS = Map.ofEntries();
+
+    public static String convert(int arabic) {
+        return null;
+    }
+
+
 }
--- src/test/java/dev/mirplatform/RomanDigitsTest.java
+++ src/test/java/dev/mirplatform/RomanDigitsTest.java
@@ -1,7 +1,27 @@
 package dev.mirplatform;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanDigitsTest {
 
+    @ParameterizedTest
+    @CsvSource({
+        "1,I",
+        "2,II",
+        "3,III",
+        "4,IV",
+        "5,V",
+        "6,VI",
+        "7,VII",
+        "8,VIII",
+        "9,IX",
+        "10,X"
+    })
+    void fromOneToTen(int arabic, String expectedRoman) {
+        String actualRoman = RomanDigits.convert(arabic);
+        assertEquals(expectedRoman, actualRoman);
+    }
 }
