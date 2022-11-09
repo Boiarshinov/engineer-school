1219a9526f5aebe272b99c9205ad4d8ca6aa9ede
boiarshinov <artem.borya@gmail.com>
2022-11-09 09:55:07 +0300

    Red playerCantBetMoreChipsThanHeHas

--- src/test/java/casino/domain/PlayerTest.java
+++ src/test/java/casino/domain/PlayerTest.java
@@ -2,6 +2,11 @@ package casino.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static casino.domain.TestDataHelper.BET_AMOUNT;
+import static casino.domain.TestDataHelper.BET_NUMBER;
+import static casino.domain.TestDataHelper.CHIPS_AMOUNT;
+import static casino.domain.TestDataHelper.defaultInitials;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
@@ -26,7 +31,7 @@ public class PlayerTest {
 
     @Test
     void playerCanLeaveTheGame() {
-        TestDataHelper.defaultInitials((casino, game, player) -> {
+        defaultInitials((casino, game, player) -> {
             game.remove(player);
 
             assertFalse(player.isInGame());
@@ -35,9 +40,22 @@ public class PlayerTest {
 
     @Test
     void playerCantLeaveGameNotParticipatingIn() {
-        TestDataHelper.defaultInitials((casino, game, player) -> {
+        defaultInitials((casino, game, player) -> {
             Game anotherGame = casino.newGame();
             assertThrows(CasinoException.class, () -> anotherGame.remove(player));
         });
+    }
+
+    @Test
+    void playerCantBetMoreChipsThanHeHas() {
+        Casino casino = new Casino();
+        Game game = casino.newGame();
+        Player player = new Player();
+        player.buy(casino, CHIPS_AMOUNT);
+        game.add(player);
+
+        assertThatThrownBy(() -> player.bet(player.getChipsAmount() + 1, BET_NUMBER))
+            .isInstanceOf(CasinoException.class)
+            .hasMessageContaining("Player cannot bet more chips than he has");
     }
 }
